66096f876a265b36b46bd15c7fc3e6531c5f9b54
Neal Brooks <nealio82@gmail.com>
2021-02-19 13:33:35 +0000

    add basic form

---
+++ src/Controller/RaffleFormController.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+final class RaffleFormController
+{
+    private FormFactoryInterface $formFactory;
+
+    private Environment $twig;
+
+    public function __construct(FormFactoryInterface $formFactory, Environment $twig)
+    {
+        $this->formFactory = $formFactory;
+        $this->twig = $twig;
+    }
+
+    public function form(Request $request): Response
+    {
+        $form = $this->formFactory->createBuilder()
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('submit', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email"
+            $data = $form->getData();
+
+            // save the data
+        }
+
+        return new Response(
+            $this->twig->render('form.html.twig', [
+                'form' => $form->createView()
+            ])
+        );
+    }
+}
